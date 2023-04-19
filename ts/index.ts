d8f52d4247261d02a6b927027205c60520e44e2c
Mohan <mohan1166.be20@chitkarauniversity.edu.in>
2023-04-19 20:47:45 +0530

    edge condition modified/added | favicon img added

--- ts/index.ts
+++ ts/index.ts
@@ -9,21 +9,21 @@ let str:string="MlPf$%&_;:ijkghEQRSTUtuvw345NO8VWXYZabcdeFGHqrs9!@#67mnIJKL012op
 let flag:boolean=true;
 
 button.addEventListener("click",()=>{
+    let password:string='';
+    let size:number=input.valueAsNumber;
     if(input.value==''){
         output.innerHTML="Enter size to generate password!!";
         flag=false;
+    }else if(size<8){
+        output.innerHTML="Size too short for strong password!!";
+        flag=false;
+    }else if(size>50){
+        output.innerHTML="Size too big for password!!";
     }else{
-        let password:string='';
-        let size:number=input.valueAsNumber;
-        if(size<8){
-            output.innerHTML="size too short for strong password!!";
-            flag=false;
-        }else{
-            for(let i=0;i<size;i++){
-                let indexRandom=Math.floor(Math.random()*str.length);
-                password+=str[indexRandom];
-                output.innerHTML=password;
-            }
+        for(let i=0;i<size;i++){
+            let indexRandom=Math.floor(Math.random()*str.length);
+            password+=str[indexRandom];
+            output.innerHTML=password;
         }
     }
-})
+});
