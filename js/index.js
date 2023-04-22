03c7849e1a5fc1beb99e723b2d7c5bc0cdd7aa27
Mohan <mohan1166.be20@chitkarauniversity.edu.in>
2023-04-22 14:18:10 +0530

    added logo,popup,copy function etc. bug fixes

--- js/index.js
+++ js/index.js
@@ -3,10 +3,17 @@
 const button = document.querySelector('.btn');
 const input = document.querySelector('#pwd-size');
 const output = document.querySelector('#pwd');
-let str = "MlPf$%&_;:ijkghEQRSTUtuvw345NO8VWXYZabcdeFGHqrs9!@#67mnIJKL012opxyzABCD?";
+const copy = document.querySelector('#copy-grp');
+const pop_up_div = document.querySelector('#pop-up');
+const close_pop_up = document.querySelector('#pop-up-btn');
+const p = document.querySelector('#p-copy');
+let str = "MlHq?/%^hEQRSTj3?uv!@KL0Y:ijkg!@)_+-+={}[#$\"\'<,ZabXgh12opxyUtEQRSTUtzABcdeF45NO8VWC$%&_;>.IJuvw345NO8VWXYZabcdeFGHqrs9!@#67DG#kMw67mnrs9lPf&*(]|:;\"$%&_;:iPfmnIJKL012opxyzABCD?";
 let flag = true;
+let password = '';
 button.addEventListener("click", () => {
-    let password = '';
+    p.innerHTML = "Copy";
+    password = '';
+    flag = true;
     let size = input.valueAsNumber;
     if (input.value == '') {
         output.innerHTML = "Enter size to generate password!!";
@@ -23,7 +30,24 @@ button.addEventListener("click", () => {
         for (let i = 0; i < size; i++) {
             let indexRandom = Math.floor(Math.random() * str.length);
             password += str[indexRandom];
-            output.innerHTML = password;
         }
+        output.innerHTML = password;
     }
+});
+copy.addEventListener('click', () => {
+    if (flag == false) {
+        return;
+    }
+    navigator.clipboard.writeText(password);
+    if (password == '') {
+        return;
+    }
+    popUp();
+    p.innerHTML = "Copied";
+});
+function popUp() {
+    pop_up_div.style.display = 'flex';
+}
+close_pop_up.addEventListener('click', () => {
+    pop_up_div.style.display = 'none';
 });
--- ts/index.ts
+++ ts/index.ts
@@ -2,14 +2,20 @@
 const button = document.querySelector('.btn') as HTMLButtonElement;
 const input=document.querySelector('#pwd-size') as HTMLInputElement;
 const output=document.querySelector('#pwd') as HTMLParagraphElement;
+const copy=document.querySelector('#copy-grp') as HTMLButtonElement;
+const pop_up_div=document.querySelector('#pop-up') as HTMLDivElement;
+const close_pop_up=document.querySelector('#pop-up-btn') as HTMLButtonElement;
+const p=document.querySelector('#p-copy') as HTMLParagraphElement;
 
-let str:string="MlPf$%&_;:ijkghEQRSTUtuvw345NO8VWXYZabcdeFGHqrs9!@#67mnIJKL012opxyzABCD?";
-
+let str:string="MlHq?/%^hEQRSTj3?uv!@KL0Y:ijkg!@)_+-+={}[#$\"\'<,ZabXgh12opxyUtEQRSTUtzABcdeF45NO8VWC$%&_;>.IJuvw345NO8VWXYZabcdeFGHqrs9!@#67DG#kMw67mnrs9lPf&*(]|:;\"$%&_;:iPfmnIJKL012opxyzABCD?";
 
 let flag:boolean=true;
+let password:string='';
 
 button.addEventListener("click",()=>{
-    let password:string='';
+    p.innerHTML="Copy"
+    password='';
+    flag=true;
     let size:number=input.valueAsNumber;
     if(input.value==''){
         output.innerHTML="Enter size to generate password!!";
@@ -23,7 +29,28 @@ button.addEventListener("click",()=>{
         for(let i=0;i<size;i++){
             let indexRandom=Math.floor(Math.random()*str.length);
             password+=str[indexRandom];
-            output.innerHTML=password;
         }
+        output.innerHTML=password;
     }
 });
+
+copy.addEventListener('click',()=>{
+    if(flag==false){
+        return;
+    }
+    navigator.clipboard.writeText(password);
+    if(password==''){
+        return;
+    }
+    popUp();
+    p.innerHTML="Copied";
+});
+
+function popUp():void{
+    pop_up_div.style.display='flex';
+
+}
+
+close_pop_up.addEventListener('click',()=>{
+    pop_up_div.style.display='none';
+})
